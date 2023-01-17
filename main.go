35123e957763c76d54a7e17ea072e48dc2197c7a
Garrett Foy <me@garrettfoy.com>
2023-01-16 18:55:28 -0600

    add Config options and start work on Class implementation

--- main.go
+++ main.go
@@ -9,20 +9,32 @@ import (
 	"net/http"
 	"regexp"
 	"sort"
+	"time"
+	"gopkg.in/yaml.v2"
 )
 
+type Config struct {
+	SchoolID string `yaml:"schoolID"`
+	Departments []string `yaml:"departmentID"`
+}
+
+//Struct that takes all GraphQL structs and combines them into something useful!
+//This sorts all professor data by CLASS rather then department or instructor.
+//Useful for when selecting a CLASS you can see the top professors for that class.
 type Class struct {
-	ClassName     string
-	NumProfessors int
-	NumReviews    int
-	Professors    []struct {
-		ProfessorName     string
-		NumReviews        int
-		AvgGrade          string
-		AvgRating         int
-		AvgWouldTakeAgain int
-		TopReview         string
-	}
+    ClassName     string
+    NumProfessors int
+    NumReviews    int
+    Professors    []Professor
+}
+
+type Professor struct {
+    ProfessorName     string
+    NumReviews        int
+    AvgGrade          string
+    AvgRating         float64
+    AvgWouldTakeAgain float64
+    TopReview         string
 }
 
 //"Final" struct that holds all teacher reviews and information
@@ -48,11 +60,10 @@ type TeacherReviews struct {
 				Grade                   string  `json:"grade"`
 				HelpfulRating           int     `json:"helpfulRating"`
 				HelpfulRatingRounded    int     `json:"helpfulRatingRounded"`
-				IWouldTakeAgain         *string `json:"iWouldTakeAgain"`
+				IWouldTakeAgain         bool `json:"iWouldTakeAgain"`
 				ID                      string  `json:"id"`
 				IsForOnlineClass        bool    `json:"isForOnlineClass"`
 				LegacyID                int     `json:"legacyId"`
-				TeacherNote             *string `json:"teacherNote"`
 			} `json:"node"`
 		} `json:"edges"`
 		PageInfo struct {
@@ -140,19 +151,59 @@ type GraphQL struct {
 	Variables map[string]interface{} `json:"variables"`
 }
 
-//Function to take a ugly RateMyProf class like MATH123 and turn it into MATH-123 :D
-//uses regex bleh
-func formatClassName(className string) string {
-	re := regexp.MustCompile("(?P<prefix>[A-Za-z]+)(?P<number>[0-9]+)")
-
-	return re.ReplaceAllString(className, "${prefix}-${number}")
-}
-
 //Helper function to reduce line complexity of the file. Simply panics at any non-null errors.
 func handleError(err error) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+//Function to load in a config file given a string filename
+func loadConfig(fileName string) Config {
+	var config Config
+
+	ymlFile, err := ioutil.ReadFile(fileName)
+	handleError(err)
+
+	err = yaml.Unmarshal(ymlFile, &config)
+	handleError(err)
+
+	return config
+}
+
+//Function to take a given date (2004-12-06 00:33:51 +0000 UTC type), and determine
+//if it is older than @months old.
+func isReviewValid(date string, months int) bool {
+    t, err := time.ParseInLocation("2006-01-02 15:04:05 -0700 UTC", date, time.UTC)
+	handleError(err)
+
+    // get the current time
+    now := time.Now()
+
+    // subtract the specified number of months from the current time
+    limit := now.AddDate(0, -months, 0)
+
+    // check if the parsed time is older than the limit
+    return t.Before(limit)
+}
+
+func isClassValid(className string) bool {
+	match, _ := regexp.MatchString(`^\D+-\d+$`, className)
+
+    return match
+}
+
+//Function to take a ugly RateMyProf class like MATH123 and turn it into MATH-123 :D
+//uses regex bleh
+func formatClassName(className string) string {
+    if className == "" {
+        return ""
+    }
+    //compile the regular expression to match letters and numbers
+    re := regexp.MustCompile("([a-zA-Z]+)([0-9]+)")
+    //replace matched letters and numbers with the matched letters, "-", and matched numbers
+    className = re.ReplaceAllString(className, "$1-$2")
+    return className
 }
 
 //Function to get all reviews based on a teacherID. Note: This is not dependant
@@ -322,7 +373,7 @@ func getAllTeachersByDepartment(departmentID string) TeachersResponse {
     }
     
     fragment TeacherSearchPagination_search_1ZLmLD on newSearch {
-      teachers(query: $query, first: 100, after: "") {
+      teachers(query: $query, first: 500, after: "") {
         edges {
           node {
             ...TeacherCard_teacher
@@ -394,6 +445,88 @@ func getAllTeachersByDepartment(departmentID string) TeachersResponse {
 	}
 }
 
+// Function to take a Teachers response, iterate through each teacher in the department,
+// fetch all reviews, then sort them into classes accordingly.
+func generateClasses(response TeachersResponse) map[string]Class {
+
+	classes := make(map[string]Class)
+
+	for _, edge := range response.Data.Search.Teachers.Edges {
+		teacherID := edge.TeacherData.ID
+
+		//Now we have the teacher ID, we gotta now fetch all of the reviews done so we can compile
+		//a map of class-reviews. 
+		teacherReviewsResponse := getAllReviewsByTeacher(teacherID, 200)
+
+		//We also want to define our Professor struct so if they teach a class, we can just add their
+		//information to the Class object.
+		professor := Professor{
+			ProfessorName: edge.TeacherData.FirstName + edge.TeacherData.LastName,
+			NumReviews: 0,
+			AvgGrade: "N/A",
+			AvgRating: edge.TeacherData.AvgRatingRounded,
+			AvgWouldTakeAgain: edge.TeacherData.WouldTakeAgainPercentRounded,
+			TopReview: "N/A",
+		}
+
+		//Now we have a response with all reviews, let's sort this into the map :D
+		for _, edge := range teacherReviewsResponse.Data.TeacherReviews.Ratings.Edges {
+			//Increment numReviews
+			professor.NumReviews = professor.NumReviews + 1
+			//Validity checks for review
+			if(isReviewValid(edge.Node.Date, 24) || !isClassValid(formatClassName(edge.Node.Class))) {
+				continue
+			}
+
+			className := formatClassName(edge.Node.Class)
+
+			//First check to see if we have the class in the map already
+			_, ok := classes[className]
+
+			if !ok {
+				//Not in map already
+				class := Class{
+					ClassName: className,
+					NumProfessors: 1,
+					NumReviews: 1,
+					Professors: []Professor{professor},
+				}
+
+				classes[className] = class
+			} else {
+				class := classes[className]
+
+				class.NumReviews = class.NumReviews + 1
+
+				//Now check if is already in the professors
+				isIn := false
+
+				for _, prof := range class.Professors {
+					if(prof.ProfessorName == professor.ProfessorName) {
+						isIn = true
+					}
+				}
+
+				if(!isIn) {
+					//Add professor to class
+					class.Professors = append(class.Professors, professor)
+					class.NumProfessors = class.NumProfessors + 1
+				} 
+
+				classes[className] = class
+			}
+		}
+	}
+
+	return classes
+}
+
+func printClasses(classes map[string]Class) {
+	for _, class := range classes {
+		fmt.Println(class)
+	}
+}
+
 //Helper function to help display a TeachersResponse.
 //Printed in order from HIGHEST rating to LOWEST rating.
 func printTeachers(response TeachersResponse) {
@@ -417,10 +550,17 @@ func printTeachers(response TeachersResponse) {
 func printReviews(response TeacherReviewsResponse) {
 
 	for _, edge := range response.Data.TeacherReviews.Ratings.Edges {
+		// if(isReviewValid(edge.Node.Date, 1)) {
+		// 	continue
+		// }
+		// if(!isClassValid(formatClassName(edge.Node.Class))) {
+		// 	continue
+		// }
+
 		fmt.Println("\n----------------------------------------")
 		fmt.Println("Review:")
-		fmt.Println("  Class: ", edge.Node.Class)
-		fmt.Println("  Review: ", formatClassName(edge.Node.Comment))
+		fmt.Println("  Class: ", formatClassName(edge.Node.Class))
+		fmt.Println("  Review: ", edge.Node.Comment)
 		fmt.Println("  Grade:  ", edge.Node.Grade)
 		fmt.Println("  Date: ", edge.Node.Date)
 		fmt.Println("----------------------------------------")
@@ -429,12 +569,19 @@ func printReviews(response TeacherReviewsResponse) {
 
 func main() {
 
-	data := getAllTeachersByDepartment("RGVwYXJ0bWVudC0xMQ==")
+	config := loadConfig("config.yml")
 
-	printTeachers(data)
+	fmt.Println("School ID is: ", config.SchoolID)
 
-	data2 := getAllReviewsByTeacher("VGVhY2hlci0xMDUwOQ==", 100)
+	// data := getAllTeachersByDepartment("RGVwYXJ0bWVudC00Ng==")
 
-	printReviews(data2)
+	// printTeachers(data)
 
+	// // data2 := getAllReviewsByTeacher("VGVhY2hlci0yNjUyNDU3", 100)
+
+	// // printReviews(data2)
+
+	// classes := generateClasses(data)
+
+	// printClasses(classes)
 }
